dc42058ebca83387da330871a0fedfed1e97b903
yurezzz13 <yurkadurka@gmail.com>
2018-05-25 23:00:49 +0300

    initial commit

---
+++ src/main/java/io/cursor/blahycarshop/BlahycarshopApplication.java
@@ -0,0 +1,12 @@
+package io.cursor.blahycarshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlahycarshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlahycarshopApplication.class, args);
+	}
+}
---
+++ src/test/java/io/cursor/blahycarshop/BlahycarshopApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cursor.blahycarshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlahycarshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
